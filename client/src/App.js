e510f5dd98351e395a673db55d907a2e0513ceea
pferlito <pferlito@gmail.com>
2020-02-27 03:55:33 -0800

    Use functional components/hooks.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, {useEffect, useState} from "react";
 import socketIOClient from "socket.io-client";
 
+/*
 class App extends Component {
   constructor() {
     super();
@@ -26,6 +27,33 @@ class App extends Component {
       </div>
     );
   }
+}
+*/
+
+function App() {
+  const [config, setConfig] = useState({
+    response: false
+  });
+  useEffect(() => {
+    const socket = socketIOClient("http://localhost:5000");
+    socket.on('connect', () => {
+      setConfig((config) => { return {...config, response: true}});
+    });
+    socket.on('message', function(msg){
+      console.log(msg);
+    });
+    socket.on('connect_error', (error) => {
+      console.log('connection error: ', error);
+      // don't keep polling
+      socket.close();
+    });
+  },[]);
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      {config.response ? <p>Chart goes here</p> : <p>Loading</p>}
+    </div>
+  );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -32,16 +32,17 @@ function updatePrices(stocks) {
   return stocks;
 }
 
+let interval;
 io.on('connection', function (socket) {
   setInterval(() => {
     stocks = updatePrices(stocks);
     const json = JSON.stringify(stocks);
     io.emit('message', [new Date(), json]);
-    console.log([new Date(), json]);
-  }, 1000)
+    //console.log([new Date(), json]);
+  }, 3000)
 });
 
 http.listen(5000, async function () {
-  await open('http://localhost:5000');
+  //await open('http://localhost:5000');
   console.log('listening on *:5000');
 });
